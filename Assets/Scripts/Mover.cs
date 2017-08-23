e5dbbe7a7f692b65d9f4f7e266ccc8d1cf8b3304
prashant <tomar.prashant162@gmail.com>
2017-08-23 14:39:05 +0530

    Pushed the new project

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    public Rigidbody rb;
+    public float speed;
+    private void Start()
+    {
+        if(speed == 0)
+        {
+            Destroy(gameObject);
+        }
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+}
